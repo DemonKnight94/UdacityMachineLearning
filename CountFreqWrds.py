86e9a81971039853898c5a92068eea610624a087
DemonKnight94 <31413732+DemonKnight94@users.noreply.github.com>
2018-03-21 18:20:05 +0530

    Update CountFreqWrds.py

--- CountFreqWrds.py
+++ CountFreqWrds.py
@@ -3,5 +3,8 @@ s="She bought a bit of butter but the butter was bitter"
 t="cat bat mat cat bat cat"
 n=input("How many most common frequent words in the sentence do you wish to display?")
 list1=s.split()
-freq=Counter(list1).most_common(n)
-print freq
+list2=s.split()
+freq1=Counter(list1).most_common(n)
+freq2=Counter(list1).most_common(n)
+print freq1
+print freq2
